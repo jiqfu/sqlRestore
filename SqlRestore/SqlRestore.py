4a4d930b345ae8cccb73d2096616e8f12acb28d8
吉福21419 <fu.ji@hand-china.com>
2021-06-30 15:58:39 +0800

    Initial commit

---
+++ SqlRestore/SqlRestore.py
@@ -0,0 +1,48 @@
+import string
+
+
+class SqlRestore():
+    SQL_START = "==>  Preparing: "
+    SQL_END = "\n"
+    PARAM_START = "==> Parameters: "
+    PARAM_END = "\n"
+    PARAM_STRING_TYPE = "String"
+    PARAM_TIMESTAMP = "Timestamp"
+    ADD_SINGLE_QUOTES_LIST = {PARAM_STRING_TYPE, PARAM_TIMESTAMP}
+    REPLACE_REG = "?"
+    SPLIT = ", "
+    SINGLE_QUOTES = "'"
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def restore(text: string):
+        # print("执行sql还原方法" + text)
+        if SqlRestore.SQL_START not in text:
+            return text
+        sql = text[text.index(SqlRestore.SQL_START) + len(SqlRestore.SQL_START):text.index(SqlRestore.SQL_END,
+                                                                                           text.index(
+                                                                                               SqlRestore.SQL_START) + len(
+                                                                                               SqlRestore.SQL_START))]
+        # print("sql is :" + sql)
+        if SqlRestore.PARAM_START not in text:
+            return sql
+        param = text[text.index(SqlRestore.PARAM_START) + len(SqlRestore.PARAM_START):text.index(SqlRestore.PARAM_END,
+                                                                                                 text.index(
+                                                                                                     SqlRestore.PARAM_START) + len(
+                                                                                                     SqlRestore.PARAM_START))]
+        # print("param is :" + param)
+        param_spit = param.split(SqlRestore.SPLIT)
+        for paramTemp in param_spit:
+            # print(paramTemp)
+            if "(" in paramTemp:
+                value = paramTemp[:paramTemp.index("(")]
+                type = paramTemp[paramTemp.index("(") + 1: paramTemp.rindex(")")]
+                # print(value)
+                # print(type)
+                if type in SqlRestore.ADD_SINGLE_QUOTES_LIST:
+                    value = '\'' + value + '\''
+                    pass
+                sql = sql.replace(SqlRestore.REPLACE_REG, value, 1)
+        return sql
---
+++ sqlRestoreUI.py
@@ -0,0 +1,39 @@
+from tkinter import *
+from tkinter.scrolledtext import *
+
+from SqlRestore.SqlRestore import SqlRestore
+
+
+class Application(Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        # 输入框
+        self.input = ScrolledText(self, width=400, undo=True, autoseparators=False)
+        # 按钮
+        self.createButton = Button(self, width=50, text="生成", relief='solid', command=self.click)
+        self.master = master
+        self.master.geometry('600x400+200+200')
+        self.master.resizable(width=False, height=False)
+        # 窗口名
+        self.master.title("sql还原工具")
+        self.pack()
+        self.create_widgets()
+
+    def click(self):
+        result = SqlRestore.restore(self.input.get('1.0', END))
+        self.input.delete(1.0, END)
+        self.input.insert(INSERT, result)
+
+    def create_widgets(self):
+        self.input.pack()
+        self.createButton.pack()
+
+
+def main():
+    root = Tk()
+    app = Application(master=root)
+    app.mainloop()
+
+
+if __name__ == '__main__':
+    main()
